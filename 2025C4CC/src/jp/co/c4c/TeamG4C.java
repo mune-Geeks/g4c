7d71f900fff6df0b4fa1607814d5d1c972278321
j-nishiki <n.junpei.2001.0319@gmail.com>
2025-05-26 12:03:06 +0900

    錦作戦 ver1.0

--- 2025C4CC/src/jp/co/c4c/TeamG4C.java
+++ 2025C4CC/src/jp/co/c4c/TeamG4C.java
@@ -49,10 +49,33 @@ public class TeamG4C {
      * @return
      */
     public Card tactics3(int turn, List<Card> p1History, List<Card> p2History) {
-        // 今はランダムで返す
-        Random random = new Random();
-        // テスト
-        return random.nextBoolean() ? Card.COOPERATE : Card.BETRAY;
+
+        // 相手の行動履歴リストをpreviousMoveに設定
+        final List<Card> previousMove = p2History;
+
+        // 相手の行動履歴リストの要素数を取得
+        final int previousMoveSize = previousMove.size();
+
+        // 怒りポイント初期値を設定
+        int angerPoints = 0;
+
+        // 最初のターンから現在のターンまでの相手の行動履歴を取得して怒りポイントを計算
+        for (int i = 0; i < previousMoveSize; i++) {
+            // 協力なら－１、裏切りなら＋１
+            angerPoints = previousMove.get(i) == Card.COOPERATE ? angerPoints -1 : angerPoints + 1;
+            // 怒りポイントが－１以下なら０を設定
+            if (angerPoints < 0) {
+                angerPoints = 0;
+            }
+            // 怒りポイントが４以上なら３を設定
+            if (angerPoints > 3) {
+                angerPoints = 3;
+            }
+        }
+
+        // 怒りポイントが２以上なら「裏切り」、２未満なら「協力」を返す
+        return angerPoints > 1 ? Card.BETRAY : Card.COOPERATE;
+
     }
 
 }
