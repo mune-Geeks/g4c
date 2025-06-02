86506d9c2a03601a2236ced2281738769b96213e
SarojSeenuan <sarojseenuan@gmail.com>
2025-06-03 00:06:50 +0900

    指摘対応　【シヌアン最終版】

--- 2025C4CC/src/jp/co/c4c/TeamG4C.java
+++ 2025C4CC/src/jp/co/c4c/TeamG4C.java
@@ -264,43 +264,34 @@ public class TeamG4C {
     }
 
     /**
-     * 作戦2　吉田作戦
+     * 作戦2 吉田作戦
      * @param turn
      * @param p1History
      * @param p2History
      * @return
      */
     public Card tactics2(int turn, List<Card> p1History, List<Card> p2History) {
-
         // フェーズ1
-        final int PHESE1_END_TURN = 50;
-
+        final int PHASE1_END_TURN = 50;
         // フェーズ2で使用するしきい値
         final int COOPERATION_THRESHOLD = 25;
-
         // フェーズ3
-        final int PHESE3_STR_TURN = 52;
-
+        final int PHASE3_STR_TURN = 52;
         // 合計協力回数
         int cooperateCount = 0;
-
         // 協力率
         double cooperateRate = 0.00;
-
         // 連続協力回数
         int consecutiveCooperateCount = 0;
-
         // 連続裏切り回数
         int consecutiveBetrayalCount = 0;
-
         // 相手の協力
         final double COOPERATION_RATE_THRESHOLD = 0.5;
-
         // 直近3ターン
         final int RECENT_TURN_COUNT = 3;
 
         // フェーズ1
-        if (turn <= PHESE1_END_TURN) {
+        if (turn <= PHASE1_END_TURN) {
             return Card.BETRAY;
         }
 
@@ -311,7 +302,7 @@ public class TeamG4C {
             }
 
             // フェーズ3のときの直近3ターンの結果をカウント
-            if (turn >= PHESE3_STR_TURN - RECENT_TURN_COUNT) {
+            if (turn >= PHASE3_STR_TURN - RECENT_TURN_COUNT) {
                 if (p2History.get(i) == Card.COOPERATE) {
                     consecutiveCooperateCount += 1;
                 } else {
@@ -321,7 +312,7 @@ public class TeamG4C {
         }
 
         // フェーズ2
-        if (turn == PHESE1_END_TURN + 1) {
+        if (turn == PHASE1_END_TURN + 1) {
             // 相手の協力回数が25回以下だった場合、裏切り
             if (cooperateCount <= COOPERATION_THRESHOLD) {
                 return Card.BETRAY;
@@ -332,7 +323,6 @@ public class TeamG4C {
 
         // フェーズ3
         cooperateRate = (double) cooperateCount / p2History.size();
-
         // 協力率が50%以上だった場合
         if (cooperateRate >= COOPERATION_RATE_THRESHOLD) {
             if (consecutiveBetrayalCount == RECENT_TURN_COUNT) {
@@ -348,14 +338,13 @@ public class TeamG4C {
     }
 
     /**
-     * 作戦3　錦作戦
+     * 作戦3 錦作戦
      * @param turn
      * @param p1History
      * @param p2History
      * @return
      */
     public Card tactics3(int turn, List<Card> p1History, List<Card> p2History) {
-
         // １ターン目は「協力」のカードを出す
         if (turn == 1) {
             return Card.COOPERATE;
